eaecc6f3a5e7d3589ffb1ca728bc3d1884b10546
aland1013 <adam.land@gmail.com>
2020-09-25 21:11:44 -0400

    added a few comments to ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,19 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.storage = []
-        self.oldest = None
+        self.storage = [] # list to store items
+        self.oldest = None # pointer to oldest item
 
     def append(self, item):
         length = len(self.storage)
         
+        # list is empty
         if length == 0:
             self.storage.append(item)
             self.oldest = 0
         
+        # list is not empty but has room for a new item
         elif length < self.capacity:
             self.storage.append(item)
         
+        # list is at capacity
         else:
             self.storage[self.oldest] = item
             if self.oldest < self.capacity - 1:
