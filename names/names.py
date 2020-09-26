45020b4866288324be0a25fd13d6a89ebf6b67bc
aland1013 <adam.land@gmail.com>
2020-09-25 22:11:27 -0400

    cleanup

--- names/names.py
+++ names/names.py
@@ -66,13 +66,6 @@ for name in names_2:
     if bst.contains(name):
         duplicates.append(name)
 
-# alternate solution using sets and built-in intersection() method
-# this code executed in 0.009 seconds on my laptop
-# s1 = set(names_1)
-# s2 = set(names_2)
-
-# duplicates = s1.intersection(s2)
-
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
@@ -81,3 +74,11 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# alternate solution using sets and built-in intersection() method
+# this code executed in 0.009 seconds on my laptop
+
+# s1 = set(names_1)
+# s2 = set(names_2)
+
+# duplicates = s1.intersection(s2)
